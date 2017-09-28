9a5278a01eb716d2f5769fcdfd7579a78d3db367
Milan Berka <berkamilanml@gmail.com>
2017-09-28 22:32:58 +0200

    Update airport_codes_extractor.py

--- bi_challenge_delay_insurance/src/airport_codes_extractor.py
+++ bi_challenge_delay_insurance/src/airport_codes_extractor.py
@@ -1,15 +1,18 @@
 """
-This script extracts `airport-code` data from `https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv`
+Following script extracts `airport-code` data from `https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv`
 """
 
 import pandas as pd
 
+# Connect to server and read the .csv with airport codes
 df = pd.read_csv('https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv')
 
+# Parse coordinates 
 coord = df['coordinates'].str.split(',', expand=True).astype(float)
 coord.columns = ['longitude','latitude']
 df['latitude'] = coord['latitude']
 df['longitude'] = coord['longitude']
-df.drop('coordinates',axis=1,inplace=True)
+df.drop('coordinates', axis=1, inplace=True)
 
+# Export the data
 df.to_csv('out/tables/airport_codes.csv', index=None, encoding='utf-8')
