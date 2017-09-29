a1c6f7d14524a8ffaa19983e7933ae3fb04e295d
Milan Berka <berkamilanml@gmail.com>
2017-09-29 18:23:51 +0200

    Enhancing

--- engineering_challenge/src/daily_trend_and_category_evaluation.sql
+++ engineering_challenge/src/daily_trend_and_category_evaluation.sql
@@ -16,21 +16,22 @@ SELECT
     TO_DATE(TRY_TO_TIMESTAMP_NTZ("requested_datetime")) as "date",
     count(*) as "requests_count"
 FROM
-  "san_francisco_data"
+   "san_francisco_data"
 WHERE
-  "service_name" = 'Damaged Property' AND
-  TO_DATE(TRY_TO_TIMESTAMP_NTZ("requested_datetime")) >= DATEADD('days',-5,CURRENT_DATE())
+   "service_name" = 'Damaged Property' AND
+   TO_DATE(TRY_TO_TIMESTAMP_NTZ("requested_datetime")) >= DATEADD('days',-5,CURRENT_DATE()) -- use for incremental addition
 GROUP BY "date";
 
 --- by closed cases
 CREATE OR REPLACE TABLE "damaged_property_closed_cases" AS
 SELECT
-  TO_DATE(TRY_TO_TIMESTAMP("closed_date")) as "date",
-  count(*) as "closed_cases_count"
+   TO_DATE(TRY_TO_TIMESTAMP_NTZ("closed_date")) as "date",
+   count(*) as "closed_cases_count"
 FROM
-  "san_francisco_data"
+   "san_francisco_data"
 WHERE
-  "service_name" = 'Damaged Property'
+   "service_name" = 'Damaged Property' AND
+   TO_DATE(TRY_TO_TIMESTAMP_NTZ("closed_date")) >= DATEADD('days',-5,CURRENT_DATE())
 GROUP BY "date";
 
 --- putting it together
@@ -43,13 +44,13 @@ FROM
   "damaged_property_closed_cases" dpc
 FULL OUTER JOIN
   "damaged_property_requests" dpr ON dpr."date" = dpc."date";
-  
+
 --- calculating percentage differences day-to-day
 CREATE OR REPLACE TABLE "damaged_property_evaluation_growth" AS
 SELECT
   t1."date",
   IFF(t2."requests_count" = 0 OR t2."requests_count" IS NULL, 0, (t1."requests_count" / t2."requests_count"-1)*100) as "requests_daily_percentage_growth",
-  IFF(t2."closed_cases_count" = 0 OR t2."closed_cases_count", 0,  (t1."closed_cases_count" / t2."closed_cases_count"-1)*100) as "closed_cases_daily_percentage_growth"
+  IFF(t2."closed_cases_count" = 0 OR t2."closed_cases_count" IS NULL, 0,  (t1."closed_cases_count" / t2."closed_cases_count"-1)*100) as "closed_cases_daily_percentage_growth"
 FROM 
 	"damaged_property_evaluation" t1 
 LEFT JOIN 
@@ -60,15 +61,15 @@ LEFT JOIN
 --- but only updates/add data 3 days back. 
 CREATE OR REPLACE TABLE "damaged_property_evaluation" AS
 SELECT
-	dpo.*,
-    dppg."requests_daily_percentage_growth",
-    dppg."closed_cases_daily_percentage_growth"
+    	dpo.*,
+    	dppg."requests_daily_percentage_growth",
+    	dppg."closed_cases_daily_percentage_growth"
 FROM
 	"damaged_property_evaluation" dpo 
 LEFT JOIN
 	"damaged_property_evaluation_growth" dppg ON dpo."date" = dppg."date"
 WHERE dpo."date" > DATEADD('days',-3,CURRENT_DATE());
-    
+
 -- II) Category distribution
 --- we create table that show us: how many requests and closed cases there was per category in last: 1 day (yesterday), 7 days and 28 days
 CREATE OR REPLACE TABLE "request_frequency_per_service_category" AS
